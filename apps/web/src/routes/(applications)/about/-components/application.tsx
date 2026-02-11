b36e6059b133ddc00f1275b1a02bdf5d3744b573
Robert Molina <rmolinamir@gmail.com>
2026-02-11 12:06:47 -0500

    chore: cleaned up creata application route factory

--- apps/web/src/routes/(applications)/about/-components/application.tsx
+++ apps/web/src/routes/(applications)/about/-components/application.tsx
@@ -3,6 +3,7 @@ import {
   LauncherIcon,
   LauncherLabel,
 } from "@acme/ui/os/launcher";
+import { User } from "lucide-react";
 import React from "react";
 import { createApplicationRoute } from "@/lib/os/create-route-application";
 
@@ -11,15 +12,14 @@ export const {
   Launcher: AboutLauncher,
   Route: AboutRoute,
 } = createApplicationRoute("/about")({
-  component: React.lazy(() =>
-    import("./about").then((module) => ({
-      default: module.About,
-    })),
-  ),
+  component: React.lazy(async () => {
+    const module = await import("./about");
+    return { default: module.About };
+  }),
   launcher: () => (
     <LauncherContent>
       <LauncherIcon className="bg-slate-200 text-slate-700 ring-black/10">
-        a
+        <User />
       </LauncherIcon>
       <LauncherLabel className="text-slate-800">about.mdx</LauncherLabel>
     </LauncherContent>
